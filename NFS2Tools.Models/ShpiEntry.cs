23df14c3fba99fac8ed9441f4843d7902b1e59d1
Horațiu Mlendea <Mlendea.Horatiu@GMail.com>
2017-08-26 18:26:50 +0300

    Created some models

---
+++ NFS2Tools.Models/ShpiEntry.cs
@@ -0,0 +1,27 @@
+﻿using System.Drawing;
+
+namespace NFS2Tools.Models
+{
+    public class ShpiEntry
+    {
+        /// <summary>
+        /// Gets the size of the block.
+        /// </summary>
+        /// <value>The size of the block.</value>
+        public int BlockSize => BitmapSize.Width * BitmapSize.Height + 16;
+
+        /// <summary>
+        /// Gets or sets the size of the bitmap.
+        /// </summary>
+        /// <value>The size in pixels of the bitmap.</value>
+        public Size BitmapSize { get; set; }
+
+        /// <summary>
+        /// Gets or sets the display location.
+        /// </summary>
+        /// <value>The location at which the bitmap will be displayed.</value>
+        public Point DisplayLocation { get; set; }
+
+        // TODO: BitmapData property
+    }
+}
---
+++ NFS2Tools.Models/StatsFile.cs
@@ -0,0 +1,29 @@
+﻿namespace NFS2Tools.Models
+{
+    public class StatsFile
+    {
+        /// <summary>
+        /// Gets or sets the name of the player.
+        /// </summary>
+        /// <value>The name of the player.</value>
+        public string PlayerName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the car identifier.
+        /// </summary>
+        /// <value>The car identifier.</value>
+        public int CarId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the time.
+        /// </summary>
+        /// <value>The time (64ths of a second).</value>
+        public int Time { get; set; }
+
+        /// <summary>
+        /// Gets or sets the type of the race.
+        /// </summary>
+        /// <value>The type of the race.</value>
+        public int RaceType { get; set; }
+    }
+}
