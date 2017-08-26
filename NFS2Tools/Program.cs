6190fc985931088912ed6543a2868b7c32f3edd0
Horațiu Mlendea <Mlendea.Horatiu@GMail.com>
2017-08-26 18:09:37 +0300

    Created the solution

---
+++ NFS2Tools/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NFS2Tools
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
