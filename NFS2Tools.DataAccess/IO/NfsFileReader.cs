adfe1cf9580cbfb67d7bce2a91741261a5512743
Horațiu Mlendea <Mlendea.Horatiu@GMail.com>
2018-04-07 20:12:17 +0300

    Created own file readers and writers

---
+++ NFS2Tools.DataAccess/IO/NfsFileReader.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.IO;
+
+namespace NFS2Tools.DataAccess.IO
+{
+    public class NfsFileReader : IDisposable
+    {
+        Stream stream;
+        bool disposed;
+
+        public string FilePath { get; }
+
+        public virtual Stream BaseStream => stream;
+
+        public NfsFileReader(string filePath, FileMode fileMode)
+        {
+            FilePath = filePath;
+
+            stream = File.Open(filePath, fileMode);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposed)
+            {
+                return;
+            }
+
+            if (disposing)
+            {
+                stream.Close();
+                stream = null;
+            }
+
+            disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        public void Close()
+        {
+            Dispose();
+        }
+
+        public byte ReadByte()
+        {
+            int b = BaseStream.ReadByte();
+
+            if (b == -1)
+            {
+                throw new EndOfStreamException();
+            }
+
+            return (byte)b;
+        }
+
+        public byte[] ReadBytes(int count)
+        {
+            if (count < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(count));
+            }
+
+            if (stream == null)
+            {
+                throw new ObjectDisposedException(null);
+            }
+
+            byte[] bytes = new byte[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                bytes[i] = ReadByte();
+            }
+
+            return bytes;
+        }
+
+        public char ReadChar() => (char)ReadByte();
+
+        public string ReadString()
+        {
+            string str = string.Empty;
+
+            while (stream.Position != stream.Length)
+            {
+                char c = ReadChar();
+
+                if (c == '\0')
+                {
+                    return str;
+                }
+
+                str += c;
+            }
+
+            return str;
+        }
+
+        public short ReadInt16()
+        {
+            byte[] bytes = ReadBytes(2);
+            Array.Reverse(bytes);
+
+            return BitConverter.ToInt16(bytes, 0);
+        }
+
+        public int ReadInt32()
+        {
+            byte[] bytes = ReadBytes(4);
+            Array.Reverse(bytes);
+
+            return BitConverter.ToInt32(bytes, 0);
+        }
+    }
+}
---
+++ NFS2Tools.DataAccess/IO/NfsFileWriter.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace NFS2Tools.DataAccess.IO
+{
+    public class NfsFileWriter : IDisposable
+    {
+        Stream stream;
+        bool disposed;
+
+        public string FilePath { get; }
+
+        public virtual Stream BaseStream => stream;
+
+        public NfsFileWriter(string filePath, FileMode fileMode)
+        {
+            FilePath = filePath;
+
+            stream = File.Open(filePath, fileMode);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposed)
+            {
+                return;
+            }
+
+            if (disposing)
+            {
+                stream.Close();
+                stream = null;
+            }
+
+            disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        public void Close()
+        {
+            Dispose();
+        }
+
+        public void WriteByte(byte value) => BaseStream.WriteByte(value);
+
+        public void WriteBytes(byte[] bytes)
+        {
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                WriteByte(bytes[i]);
+            }
+        }
+
+        public void WriteChar(char value) => WriteByte((byte)value);
+
+        public void WriteString(string value)
+        {
+            byte[] strBytes = value.Select(Convert.ToByte).ToArray();
+
+            WriteBytes(strBytes);
+            WriteByte(0);
+        }
+
+        public void WriteInt16(short value)
+        {
+            byte[] bytes = BitConverter.GetBytes(value);
+            Array.Reverse(bytes);
+
+            WriteBytes(bytes);
+        }
+
+        public void WriteInt32(int value)
+        {
+            byte[] bytes = BitConverter.GetBytes(value);
+            Array.Reverse(bytes);
+
+            WriteBytes(bytes);
+        }
+    }
+}
